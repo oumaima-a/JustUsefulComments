81eca3fac2223be708d2c83763896ea80104ebfb
oumaima-a <76006484+oumaima-a@users.noreply.github.com>
2026-02-27 13:02:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ try:
         st.error(f"حدث خطأ أثناء فحص الموديلات: {e}")
 
     # استخدام الموديل المستقر
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-flash')
 except Exception as e:
     st.error("لم يتم العثور على مفتاح API في الإعدادات السرية.")
     st.stop()
